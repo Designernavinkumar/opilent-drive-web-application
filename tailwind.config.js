78bdefd8d5b7b997351d8d26f89c1631b1c6f7df
navin kumar <designernavinkumar@gmail.com>
2024-12-01 21:12:10 +0530

    feat-commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+        '1': '#3CABA4',
+        '2': '##FFFFFF',
+        },
+        black: {
+        '1':'#1A1A1A',
+        '2':'#333333',
+        },
+        Gray :{
+        '1':'#F7F7F7',
+        '2':'#E6E6E6',
+        '3':'#CCCCCC',
+        '4':'#B3B3B3',
+        '5':'#666666',
+        },
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        noto: ['Work Sans', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
